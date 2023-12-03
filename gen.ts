84c18be7939983d29842fc6ee473f9edcfe1d536
Gnlow <gnlow@protonmail.com>
2023-12-03 15:38:09 +0900

    brrr

---
+++ gen.ts
@@ -0,0 +1,106 @@
+import { parse } from "https://deno.land/std@0.208.0/yaml/mod.ts"
+
+type Data = Record<string, Record<string, string[]>>
+
+const init = parse(await Deno.readTextFile("data/entryjs/o.yaml")) as Data
+
+const result = Object.entries(init)
+    .map(([type, v]) =>
+        Object.entries(v)
+            .map(([category, list]) =>
+                list.map(name => ({
+                    name,
+                    category,
+                    type,
+                    stat: {
+                        // entryjs: "O",
+                    } as Record<string, string>
+                }))
+            )
+    ).flat().flat().flat()
+
+const projectList = [] as string[]
+
+for await (const { name: project } of Deno.readDir("data/")) {
+    projectList.push(project)
+    for await (const { name: fileName } of Deno.readDir(`data/${project}`)) {
+        console.log(fileName)
+        const src = parse(await Deno.readTextFile(`data/${project}/${fileName}`)) as Data
+
+        Object.entries(src)
+            .forEach(([type, v]) =>
+                Object.entries(v)
+                    .forEach(([category, list]) =>
+                        list.forEach(name_ => {
+                            name_ = name_.replace("*", "")
+                            const target = result.find(({name}) => name == name_)
+                            if (!target) {
+                                console.log("Invalid feature name", {
+                                    project,
+                                    fileName,
+                                    type,
+                                    category,
+                                    name,
+                                })
+                            } else {
+                                target.stat[project] = fileName.replace(".yaml", "") + (
+                                    name_.endsWith("*")
+                                        ? "*"
+                                        : ""
+                                )
+                            }
+                        })
+                    )
+            )
+    }
+}
+
+const meta = {} as Record<string /* type */,
+    Record<string /* featureName */,
+        { template: string }
+    >
+>
+
+for await (const dir of Deno.readDir("meta/")) {
+    meta[dir.name.replace(".yaml", "")] = parse(await Deno.readTextFile(`meta/${dir.name}`)) as Record<string /* featureName */,
+        { template: string }
+    >
+}
+
+/*
+<tr>
+    <th scope="row">{시작하기 버튼을 클릭했을 때}</th>
+    <td o>0.1.0</td>
+    <td x>x</td>
+</tr>
+*/
+
+const gen = result.map(
+    ({name: name_, category, type, stat}) => `
+        <tr>
+            <th scope="row">${
+                meta[type][name_]?.template
+            }</th>
+            ${
+                projectList.map(project => {
+                    if (stat[project]) {
+                        if (stat[project].endsWith("*")) {
+                            return `<td partial>${stat[project]}</td>`
+                        } else {
+                            return `<td o>${stat[project]}</td>`
+                        }
+                    } else {
+                        return "<td x>x</td>"
+                    }
+                })
+            }
+        </tr>
+    `
+).join("\n")
+
+const template = await Deno.readTextFile("./index.html")
+
+export default template.replace(
+    /<!-- Here -->.*<!-- There -->/s,
+    gen,
+)
--- mod.ts
+++ mod.ts
@@ -1,28 +1,11 @@
 import { Hono } from "https://deno.land/x/hono@v3.10.4/mod.ts"
 
+import view from "./gen.ts"
+
 const app = new Hono()
 
 app.get("/", c => 
-    c.html(`
-        <!doctype html>
-        <html>
-            <head>
-            </head>
-            <body>
-                <table>
-                    <tr>
-                        <td>1</td>
-                        <td>2</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>4</td>
-                    </tr>
-                </table>
-                <script src="https://unpkg.com/adorable-css@1.4.3"></script>
-            </body>
-        </html>
-    `)
+    c.html(view)
 )
 
 Deno.serve({ port: 8081 }, app.fetch)
